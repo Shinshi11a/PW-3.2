57168bcaa8eb33481b254822e8765b40b89ef80c
Shinshi11a <pro100vanyok2016@gmail.com>
2023-03-14 19:28:29 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿public class SmsMessage
+{
+    private const int MaxLength = 250;
+    private const int MaxTaxedLength = 65;
+    private const decimal BasePrice = 1.5m;
+    private const decimal TaxPrice = 0.5m;
+
+    private string messageText;
+    private decimal price;
+
+    public SmsMessage(string messageText)
+    {
+        MessageText = messageText;
+        Price = CalculatePrice(messageText);
+    }
+
+    public string MessageText
+    {
+        get { return messageText; }
+        set { messageText = value.Substring(0, Math.Min(value.Length, MaxLength)); }
+    }
+
+    public decimal Price
+    {
+        get { return price; }
+        set { price = value; }
+    }
+
+    public static SmsMessage CreateFromUserInput()
+    {
+        Console.WriteLine("Введите текст сообщения:");
+        string messageText = Console.ReadLine();
+        return new SmsMessage(messageText);
+    }
+
+    private static decimal CalculatePrice(string messageText)
+    {
+        int length = Math.Min(messageText.Length, MaxLength);
+        decimal price = BasePrice;
+
+        if (length > MaxTaxedLength)
+        {
+            int taxedLength = length - MaxTaxedLength;
+            price += taxedLength * TaxPrice;
+        }
+
+        return price;
+    }
+}
